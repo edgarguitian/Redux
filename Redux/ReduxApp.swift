f00a0e1e1dcf1d427c7236c99498ee7af1454f2f
Edgar <edguitian@imagine800.es>
2024-01-04 16:54:04 +0100

    Initial Commit

---
+++ Redux/ReduxApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReduxApp.swift
+//  Redux
+//
+//  Created by Edgar Guitian Rey on 4/1/24.
+//
+
+import SwiftUI
+
+@main
+struct ReduxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
